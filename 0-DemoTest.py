111aa92c7686b5cd5a43d30a113167603572ebc3
Aarti Khatri <aarti.khatri@hotmail.co.uk>
2016-11-17 20:45:38 +0000

    Demo Test Complete
    Binary Gap:
    for loop
    if loop

---
+++ 0-DemoTest.py
@@ -0,0 +1,66 @@
+"""
+This is a demo task.
+
+A zero-indexed array A consisting of N integers is given. An equilibrium index of this array is any integer P such that 0 ≤ P < N and the sum of elements of lower indices is equal to the sum of elements of higher indices, i.e. 
+A[0] + A[1] + ... + A[P−1] = A[P+1] + ... + A[N−2] + A[N−1].
+Sum of zero elements is assumed to be equal to 0. This can happen if P = 0 or if P = N−1.
+
+For example, consider the following array A consisting of N = 8 elements:
+
+  A[0] = -1
+  A[1] =  3
+  A[2] = -4
+  A[3] =  5
+  A[4] =  1
+  A[5] = -6
+  A[6] =  2
+  A[7] =  1
+P = 1 is an equilibrium index of this array, because:
+
+A[0] = −1 = A[2] + A[3] + A[4] + A[5] + A[6] + A[7]
+P = 3 is an equilibrium index of this array, because:
+
+A[0] + A[1] + A[2] = −2 = A[4] + A[5] + A[6] + A[7]
+P = 7 is also an equilibrium index, because:
+
+A[0] + A[1] + A[2] + A[3] + A[4] + A[5] + A[6] = 0
+and there are no elements with indices greater than 7.
+
+P = 8 is not an equilibrium index, because it does not fulfill the condition 0 ≤ P < N.
+
+Write a function:
+
+def solution(A)
+
+that, given a zero-indexed array A consisting of N integers, returns any of its equilibrium indices. The function should return −1 if no equilibrium index exists.
+
+For example, given array A shown above, the function may return 1, 3 or 7, as explained above.
+
+Assume that:
+
+N is an integer within the range [0..100,000];
+each element of array A is an integer within the range [−2,147,483,648..2,147,483,647].
+Complexity:
+
+expected worst-case time complexity is O(N);
+expected worst-case space complexity is O(N), beyond input storage (not counting the storage required for input arguments).
+Elements of input arrays can be modified.
+"""
+
+def solution(A):
+	#LHS sum is 0 as we will begin itteration from A[0]
+    left_sum = 0
+    #RHS sum is set to total sum to begin with
+    right_sum = sum(A)
+    for i in xrange(len(A)):
+    	#Subtract A[i] from RHS to show we are moving the equilibrium by 1 position
+        right_sum -= A[i]
+        #RHS = LHS, return the position
+        if left_sum == right_sum:
+            return i
+        #Add the equilibrium value to the left sum as we need to move to the next position
+        #Hence the equilibrium position ==> value needs to change
+        left_sum += A[i]
+    #If there's no equilibrium, return -1
+    else: 
+        return -1
